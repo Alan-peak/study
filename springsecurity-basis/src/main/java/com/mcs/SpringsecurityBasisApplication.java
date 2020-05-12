d7962b499aaad184d4c68c8fcb5daf68840ff8d8
Alan-peak <18813291795@163.com>
2020-05-13 00:27:06 +0800

    初始化 springsecurity-basis 环境

---
+++ springsecurity-basis/src/main/java/com/mcs/SpringsecurityBasisApplication.java
@@ -0,0 +1,13 @@
+package com.mcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityBasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityBasisApplication.class, args);
+	}
+
+}
